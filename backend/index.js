4f60666ac240ac16d44d5526bcb322dd2166a041
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-09 22:27:40 +0530

    inital-setup-db added

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+
+const mongoose = require("mongoose")
+require('dotenv').config();
+const express = require("express")
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const cors  = require("cors")
+
+
+//DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(() => {
+    console.log("DB connected");
+    }).catch(() => {
+        console.log("DB not connected");
+    })
+
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
