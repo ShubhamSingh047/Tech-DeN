972f69c07458383a0321cf796bc35c8c95cf5cbb
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-13 22:16:20 +0530

    profile-base 90% donegit add .

--- src/Profile/ProfileBase.js
+++ src/Profile/ProfileBase.js
@@ -3,6 +3,8 @@ import "./profile.css";
 
 import notify from "../images/notify.svg";
 import search from "../images/search.svg";
+import setting from "../images/setting.svg";
+import dashboard from "../images/dashboard.svg";
 
 function ProfileBase({ children }) {
   return (
@@ -15,16 +17,26 @@ function ProfileBase({ children }) {
           </div>
           <div className="topleftbar">
             <div className="searchbar">
-              <input type="text" placeholder="Search..." />
+              <input type="text" placeholder={`  search...`} />
             </div>
             <div className="notification">
-              N{/* <img width="30px" height="30px" src="" alt="" /> */}
+              <img src={notify} alt="" />
             </div>
-            <div className="profile-pic">{/* <img src="" alt="" /> */}P</div>
+            <div className="profile-pic"></div>
           </div>
         </div>
       </div>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <div className="logoTD">TD</div>
+        <div className="dashB">
+          <img src={dashboard} alt="" />
+        </div>
+        <div className="setting">
+          <img src={setting} alt="" />
+        </div>
+      </div>
+
+      <div className="children">Start Here....{children}</div>
     </div>
   );
 }
