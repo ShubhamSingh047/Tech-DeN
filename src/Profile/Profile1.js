6be9efaa81d0cc5c41f2d7ab2211f9ed5c85c723
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-15 13:56:25 +0530

    general Info 80%done

--- src/Profile/Profile1.js
+++ src/Profile/Profile1.js
@@ -1,9 +1,77 @@
 import React from "react";
 import ProfileBase from "./ProfileBase";
+import "./profile.css";
 function Profile1() {
   return (
     <ProfileBase>
-      <h2>General Information</h2>
+      <div className="general">
+        <div className="leftside">
+          <div className="avatarProfile"></div>
+          <div className="profileName">Arijit Singh</div>
+          <div className="professionName">Playback singer</div>
+          <div className="generalInfo-panel">
+            <div className="iconss"> </div>
+            <div className="text-info-panel">
+              <div className="panel-name-general">General Information</div>
+              <div className="panel-subname">Profile</div>
+            </div>
+          </div>
+          <div className="security-panel">
+            <div className="iconss"> </div>
+            <div className="text-info-panel">
+              <div className="panel-name-security">Security</div>
+              <div className="panel-subname">Password</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rightside">
+          {/* bro krte hai yr */}
+          <div className="topname">General Information</div>
+          <div className="detailBox">
+            <div className="detailField">
+              <div className="subHeader">Name</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Username(optional)</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Email</div>
+              <div className="fieldValue">modanwalvishal@gmail.com</div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Empty</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Department</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Designation</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">LinkedIn Profile URL</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+            <div className="detailField">
+              <div className="subHeader">Github Profile URL</div>
+              <div className="fieldValue"></div>
+              <div className="fieldIcon"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </ProfileBase>
   );
 }
