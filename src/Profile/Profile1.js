1b4af09c49494dc734434c832d9787544cb9efb1
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-16 23:06:25 +0530

    general-needed to-add-with-backend

--- src/Profile/Profile1.js
+++ src/Profile/Profile1.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ProfileBase from "./ProfileBase";
 import "./profile.css";
+import user_1 from "../images/user_1.svg";
+import linkedIn from "../images/linkedIn.svg";
+import notify from "../images/notify.svg";
+import envelope from "../images/envelope.svg";
+import Group01 from "../images/Group 30250.svg";
+import portfolio from "../images/portfolio.svg";
+import search from "../images/search.svg";
+import setting from "../images/setting.svg";
+import roundeUser from "../images/roundeUser.svg";
+
 function Profile1() {
   return (
     <ProfileBase>
       <div className="general">
         <div className="leftside">
-          <div className="avatarProfile"></div>
-          <div className="profileName">Arijit Singh</div>
-          <div className="professionName">Playback singer</div>
+         <div className="avatarProfile"></div>
+        <div className="profileName">Vishal M</div>
+          <div className="professionName">React-dev</div>
           <div className="generalInfo-panel">
             <div className="iconss"> </div>
             <div className="text-info-panel">
@@ -31,43 +41,73 @@ function Profile1() {
           <div className="detailBox">
             <div className="detailField">
               <div className="subHeader">Name</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">
+                  <img src={user_1} alt="" />
+                </div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">Username(optional)</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">
+                  <img src={user_1} alt="" />
+                </div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">Email</div>
-              <div className="fieldValue">modanwalvishal@gmail.com</div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue">modanwalvishal@gmail.com</div>
+                <div className="fieldIcon">
+                  <img src={envelope} alt="" />
+                </div>
+              </div>
             </div>
-            <div className="detailField">
-              <div className="subHeader">Empty</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+            
+            <div className="detailFields">
+              <div className="subHeader"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon"></div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">Department</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">
+                  <img src={portfolio} alt="" />
+                </div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">Designation</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">
+                  {" "}
+                  <img src={portfolio} alt="" />
+                </div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">LinkedIn Profile URL</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">
+                  <img src={linkedIn} alt="" />
+                </div>
+              </div>
             </div>
             <div className="detailField">
               <div className="subHeader">Github Profile URL</div>
-              <div className="fieldValue"></div>
-              <div className="fieldIcon"></div>
+              <div className="mainItem">
+                <div className="fieldValue"></div>
+                <div className="fieldIcon">G</div>
+              </div>
             </div>
           </div>
         </div>
